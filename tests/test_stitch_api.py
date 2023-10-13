8fcc41334a3e8398db5b95cb7b51fed157083523
Charlie Marshak <charlie.z.marshak@jpl.nasa.gov>
2023-10-13 16:06:13 -0700

    include tests and templates for PRs

--- tests/test_stitch_api.py
+++ tests/test_stitch_api.py
@@ -5,3 +5,9 @@ def test_api():
     bounds = [-120.45, 34.85, -120.15, 35.15]
     X, p = get_raster_from_tiles(bounds, tile_shortname='esa_world_cover_2021')
     assert len(X.shape) == 3
+
+    X, p = get_raster_from_tiles(bounds, tile_shortname='esa_world_cover_2020')
+    assert len(X.shape) == 3
+
+    X, p = get_raster_from_tiles(bounds, tile_shortname='peckel_water_occ_2021')
+    assert len(X.shape) == 3
