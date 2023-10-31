c7260a4829585ab5bfba409a0037bf660032b48c
Charlie Marshak <charlie.z.marshak@jpl.nasa.gov>
2023-10-31 14:48:28 -0700

    hansen mosaics and merge performance jump

--- tests/test_stitch_api.py
+++ tests/test_stitch_api.py
@@ -1,7 +1,12 @@
+import random
+
+import pytest
+
 from tile_stitcher import get_raster_from_tiles
+from tile_stitcher.stitcher import HANSEN_MOSAIC_YEARS
 
 
-def test_api():
+def test_esa_world_cover():
     bounds = [-120.45, 34.85, -120.15, 35.15]
     X, p = get_raster_from_tiles(bounds, tile_shortname='esa_world_cover_2021')
     assert len(X.shape) == 3
@@ -9,5 +14,18 @@ def test_api():
     X, p = get_raster_from_tiles(bounds, tile_shortname='esa_world_cover_2020')
     assert len(X.shape) == 3
 
+
+def test_pekel_water_occ():
+    bounds = [-120.45, 34.85, -120.15, 35.15]
     X, p = get_raster_from_tiles(bounds, tile_shortname='peckel_water_occ_2021')
+    assert len(X.shape) == 3
+
+
+@pytest.mark.parametrize("year",
+                         [HANSEN_MOSAIC_YEARS[k] for k in [0, 2, 4, 6, 10]]  # random.sample(HANSEN_MOSAIC_YEARS, 3)
+                         )
+def test_hansen_datasets(year):
+    # Note only getting 1 tile - these are large datasets!
+    bounds = [-120.45, 34.85, -120.15, 34.95]
+    X, p = get_raster_from_tiles(bounds, tile_shortname='hansen_annual_mosaic', year=year)
     assert len(X.shape) == 3
--- tile_stitcher/stitcher.py
+++ tile_stitcher/stitcher.py
@@ -1,92 +1,138 @@
 from functools import lru_cache
 from pathlib import Path
-from tempfile import NamedTemporaryFile
 
 import geopandas as gpd
-import rasterio.mask
+import rasterio
 from dem_stitcher.geojson_io import read_geojson_gzip
-from osgeo import gdal
+from dem_stitcher.merge import merge_tile_datasets_within_extent
+from rasterio.errors import RasterioIOError
 from shapely.geometry import box
 
-from .exceptions import TilesetNotSupported
+from .exceptions import NoTileCoverage, TilesetNotSupported
 from .tile_model import TILE_SCHEMA
 
 DATA_DIR = Path(__file__).resolve().parent / 'data'
 GEOJSON_DICT = {
     'peckel_water_occ_2021': 'pekel_water_occurrence_2021.geojson.zip',
     'esa_world_cover_2020': 'esa_world_cover_2020.geojson.zip',
-    'esa_world_cover_2021': 'esa_world_cover_2021.geojson.zip'
+    'esa_world_cover_2021': 'esa_world_cover_2021.geojson.zip',
+    'hansen_annual_mosaic': 'hansen_landsat_mosaic_2022.geojson.zip'
 }
 DATASET_SHORTNAMES = list(GEOJSON_DICT.keys())
 
-
-gdal.UseExceptions()
+DATASETS_WITH_YEAR = ['hansen_annual_mosaic']
+HANSEN_MOSAIC_YEARS = [2000] + list(range(2013, 2023))
+CURRENT_HANSEN_VERSION = 10
+CURRENT_HANSEN_YEAR = 2022
 
 
 @lru_cache
-def get_tile_data(tile_key: str) -> gpd.GeoDataFrame:
+def get_tile_data(tile_key: str, year: int = None) -> gpd.GeoDataFrame:
     if tile_key not in DATASET_SHORTNAMES:
         raise TilesetNotSupported
     geojson_name = GEOJSON_DICT[tile_key]
     geojson_path = DATA_DIR / geojson_name
     df_tiles = read_geojson_gzip(geojson_path)
+
+    if (year is not None):
+        if tile_key not in DATASETS_WITH_YEAR:
+            raise ValueError('Year is only supported '
+                             f'with {DATASETS_WITH_YEAR}')
+        if tile_key == 'hansen_annual_mosaic':
+
+            def update_hansen_landsat_mosaic_url_p(url):
+                return update_hansen_landsat_mosaic_url(url, year)
+            df_tiles.url = df_tiles.url.map(update_hansen_landsat_mosaic_url_p)
+        else:
+            raise NotImplementedError
     return df_tiles
 
 
-def crop_raster_from_bounds(image_path: Path | str, bounds: list[float]) -> tuple:
-    """Bounds must be in lon/lat xmin, ymin, xmax, ymax (epsg:4326)"""
-    geo = box(*bounds)
-    with rasterio.open(image_path) as src:
-        cropped_image, cropped_transform = rasterio.mask.mask(src, [geo], crop=True)
-        p = src.profile
+def update_hansen_landsat_mosaic_url(url: str, year: int):
+    if year not in HANSEN_MOSAIC_YEARS:
+        raise ValueError(f'Year must be in {", ".join(HANSEN_MOSAIC_YEARS)}')
 
-    p.update({"driver": "GTiff",
-              "height": cropped_image.shape[1],
-              "width": cropped_image.shape[2],
-              "transform": cropped_transform,
-              "compress": "lzw"})
+    if year == 2000:
+        url_updated = url.replace('last', 'first')
+    elif year <= 2015:
+        # Gets the "last_00N_040W.tif" portion of the url
+        url_end = url[-17:]
+        url_updated = ('https://storage.googleapis.com/earthenginepartners-hansen/'
+                       f'GFC{year}/Hansen_GFC{year}_{url_end}')
+    else:
+        year_diff = CURRENT_HANSEN_YEAR - year
+        version_updated = CURRENT_HANSEN_VERSION - year_diff
+        url_updated = url.replace(str(CURRENT_HANSEN_YEAR),
+                                  str(year))
+        url_updated = url_updated.replace(f'v1.{CURRENT_HANSEN_VERSION}',
+                                          f'v1.{version_updated}')
 
-    return cropped_image, p
+    return url_updated
 
 
-def build_vrt_from_tile_df(extent: list[float],
-                           out_vrt_path: Path | str,
-                           df_tiles: gpd.GeoDataFrame
-                           ) -> Path:
+
+def get_urls_from_tile_df(extent: list[float],
+                          df_tiles: gpd.GeoDataFrame) -> list[str]:
     bbox = box(*extent)
     ind_inter = df_tiles.geometry.intersects(bbox)
     df_subset = df_tiles[ind_inter].reset_index(drop=True)
-    ds = gdal.BuildVRT(str(out_vrt_path), df_subset.url.tolist())
-    del ds
-    return out_vrt_path
+    if df_subset.empty:
+        raise NoTileCoverage('There are no tiles over the requested area')
+    urls = df_subset.url.tolist()
+    return urls
+
+
+def get_additional_tile_metadata(urls: list[str],
+                                 max_tile_tries: int = 10) -> dict:
+    """Some tile sets may have missing data when they should not. Ideally we
+    can remove said tiles from dataframe. However, in the case of Hansen
+    mosiacs, these errors seem to be year-to-year e.g. 2017 so are doing this
+    less elegant error handling"""
+    for k, url in enumerate(urls):
+        if k == max_tile_tries:
+            metadata = {}
+            break
+        try:
+            with rasterio.open(url) as ds:
+                tags = ds.tags()
+                try:
+                    cmap = {k+1: ds.colormap(k+1) for k in range(ds.count)}
+                # no colormap yields a ValueError in Rasterio
+                except ValueError:
+                    cmap = {}
+            metadata = {'tags': tags,
+                        'colormap': cmap}
+            break
+        except RasterioIOError:
+            continue
+    return metadata
 
 
 def get_raster_from_tiles(extent: list[float],
                           tile_shortname: str = None,
-                          df_tiles: gpd.GeoDataFrame = None) -> tuple:
+                          df_tiles: gpd.GeoDataFrame = None,
+                          year: int = None) -> tuple:
 
     if (tile_shortname is None) and (df_tiles is None):
-        raise ValueError('Either "tile_shortname" or "df_tiles" must be provided')
+        raise ValueError('Either "tile_shortname" or '
+                         '"df_tiles" must be provided')
 
     if (tile_shortname is not None) and (df_tiles is not None):
-        raise ValueError('"tile_shortname" and "df_tiles" cannot both be provided')
+        raise ValueError('"tile_shortname" and "df_tiles" cannot '
+                         'both be provided')
 
     if isinstance(tile_shortname, str):
-        df_tiles = get_tile_data(tile_shortname)
+        df_tiles = get_tile_data(tile_shortname, year=year)
 
     df_tiles = TILE_SCHEMA.validate(df_tiles)
 
-    url_0 = df_tiles.url[0]
-    with rasterio.open(url_0) as ds:
-        tags = ds.tags()
-        cmap = {k+1: ds.colormap(k+1) for k in range(ds.count)}
+    urls = df_tiles.url.tolist()
+    tile_metadata = get_additional_tile_metadata(urls)
 
-    with NamedTemporaryFile(suffix='.vrt') as tmp_vrt:
-        build_vrt_from_tile_df(extent, tmp_vrt.name, df_tiles)
-        X, p = crop_raster_from_bounds(tmp_vrt,
-                                       extent)
+    urls_subset = get_urls_from_tile_df(extent, df_tiles)
+    X_merged, p_merged = merge_tile_datasets_within_extent(urls_subset,
+                                                           extent)
 
     # Are stored in the profile for provenance
-    p['colormap'] = cmap
-    p['tags'] = tags
-    return X, p
+    p_merged.update(**tile_metadata)
+    return X_merged, p_merged
