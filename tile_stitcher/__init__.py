8c71f6efeccdff25f25eddd21abe1ef2e5a29459
Charlie Marshak <charlie.z.marshak@jpl.nasa.gov>
2023-12-04 10:17:49 -0800

    add version catch

--- tile_stitcher/__init__.py
+++ tile_stitcher/__init__.py
@@ -1,3 +1,16 @@
+import warnings
+
+from importlib_metadata import PackageNotFoundError, version
+
+try:
+    __version__ = version(__name__)
+except PackageNotFoundError:
+    __version__ = None
+    warnings.warn('package is not installed!\n'
+                  'Install in editable/develop mode via (from the top of this repo):\n'
+                  '   python -m pip install -e .\n', RuntimeWarning)
+
+
 from .stitcher import DATASET_SHORTNAMES, get_raster_from_tiles
 
 __all__ = ['get_raster_from_tiles', 'DATASET_SHORTNAMES']
