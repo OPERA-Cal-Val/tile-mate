40ed8ae0463ff02952daf874d54888af12099b1e
Simran S Sangha <sssangha@ucla.edu>
2024-12-05 17:09:19 -0800

    Use internal function to preserve validation

--- src/tile_mate/tile_model.py
+++ src/tile_mate/tile_model.py
@@ -1,11 +1,6 @@
 import geopandas as gpd
 from pandera import Column, DataFrameSchema
 
-# Add support for GeometryDtype
-pandas_engine.Engine.dtype = staticmethod(
-    lambda dtype: dtype if dtype == gpd.array.GeometryDtype else None
-)
-
 TILE_SCHEMA = DataFrameSchema(
     {
         'tile_id': Column(str, required=True),
@@ -13,6 +8,6 @@ TILE_SCHEMA = DataFrameSchema(
         'year': Column(int, required=False),
         'temporal_baseline_days': Column(int, required=False),
         'season': Column(str, required=False),
-        'geometry': Column(gpd.array.GeometryDtype, required=True),
+        'geometry': Column("geometry", required=True),
     }
 )
